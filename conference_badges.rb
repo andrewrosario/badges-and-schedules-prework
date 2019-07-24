e48607ad3218f15766599e542ebecb694aacd95c
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 14:22:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,6 @@
 # Write your code here.
 
 def batch_badge_creator(names_list)
-  names_list.each do 
-   "Hello, my name is #{name}."
+  names_list.each do { |name| "Hello, my name is #{name}."
 end
 
