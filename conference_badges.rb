e64bae374d74f7e1812f8106a44c59ac865f7e54
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-24 14:27:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,7 @@ end
 def batch_badge_creator(names_list)
   names_list.each do |name| 
     badges_array = []
+    puts badge_maker(name)
     badges_array.push(badge_maker(name))
     return badges_array
   end
